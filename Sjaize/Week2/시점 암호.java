93cce744f9c9544352395c3f80cc286042a53540
JongHyun Oh <sjaiz1717@gmail.com>
2025-04-05 17:27:42 +0900

    [PGS] 시점 암호 / Lv. 1
    https://school.programmers.co.kr/learn/courses/30/lessons/12926?language=java

---
+++ Sjaize/Week2/시점 암호.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String s, int n) {
+        char[] arr = s.toCharArray();
+        
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == ' ') continue;
+            if (Character.isUpperCase(arr[i])) {
+                arr[i] = (char)('A' + (arr[i] - 'A' + n) % 26);
+            }
+            if (Character.isLowerCase(arr[i])) {
+                arr[i] = (char)('a' + (arr[i] - 'a' + n) % 26);
+            }
+        }
+        return new String(arr);
+    }
+}
