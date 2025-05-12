05ef59cc301d63620695229babb4ddd92de1325a
JinsuShin (SHINJINSU) <jsshin8128@gmail.com>
2025-05-13 00:03:33 +0900

    Add files via upload

---
+++ jsshin8128/3진법뒤집기.cpp
@@ -0,0 +1,21 @@
+﻿#include <string>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    vector<int> v;
+
+    while (n > 0) {
+        v.push_back(n % 3);
+        n /= 3;
+    }
+
+    for (int i = v.size() - 1; i >= 0; i--) {
+        answer += v[v.size() - i - 1] * int(pow(3, i));
+    }
+
+    return answer;
+}
---
+++ jsshin8128/문자열다루기기본.cpp
@@ -0,0 +1,21 @@
+﻿#include <string>
+#include <vector>
+#include <cctype>
+
+using namespace std;
+
+bool solution(string s) {
+    bool answer = true;
+
+    if (s.length() != 4 && s.length() != 6) {
+        return false;
+    }
+
+    for (int i = 0; i < s.length(); i++) {
+        if (!isdigit(s[i])) {
+            return false;
+        }
+    }
+
+    return answer;
+}
